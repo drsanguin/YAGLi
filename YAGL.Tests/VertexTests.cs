70c2054d5edf9522ccc8111c9a8e2366786899af
thomasperez <thomas_perez@orange.fr>
2017-02-05 20:25:28 +0100

    Vertex now use a generic type to store data instead of a string label and override ToString

--- YAGL.Tests/VertexTests.cs
+++ YAGL.Tests/VertexTests.cs
@@ -8,25 +8,25 @@ namespace YAGL.Tests
     public class VertexTests
     {
         [Test]
-        public void Vertex_Label_should_return_string_Empty_if_no_label_has_been_passed_to_the_constructor()
+        public void Vertex_Constructor_should_throw_a_ArgumentNullException_if_the_data_is_null()
         {
-            Vertex vertex = new Vertex();
-
-            Check.That(vertex.Label).IsEqualTo(string.Empty);
+            Check.ThatCode(() => new Vertex<object>(null)).Throws<ArgumentNullException>();
         }
 
         [Test]
-        public void Vertex_Label_should_return_the_label_that_has_been_passed_to_the_constructor()
+        public void Vertex_Data_should_return_the_same_data_passed_in_the_constructor()
         {
-            Vertex vertex = new Vertex("42");
+            Vertex<string> vertex = new Vertex<string>("42");
 
-            Check.That(vertex.Label).IsEqualTo("42");
+            Check.That(vertex.Data).IsEqualTo("42");
         }
 
         [Test]
-        public void Vertex_constructor_should_throws_a_a_ArgumentNullException_if_the_label_passed_to_the_constructo_is_null()
+        public void Vertex_ToString_should_return_the_same_string_returned_by_the_ToString_of_the_data_stored_in_the_Vertex()
         {
-            Check.ThatCode(() => new Vertex(null)).Throws<ArgumentNullException>();
+            Vertex<string> vertex = new Vertex<string>("42");
+
+            Check.That(vertex.ToString()).IsEqualTo("42");
         }
     }
 }
--- YAGL/Vertex.cs
+++ YAGL/Vertex.cs
@@ -3,40 +3,43 @@
 namespace YAGL
 {
     /// <summary>
-    /// The class <see cref="Vertex"/> model the fundamental unit of wich graphs are formed in Graph Theory.
+    /// The class <see cref="Vertex{TData}"/> model the fundamental unit of wich graphs are formed in Graph Theory.
     /// </summary>
-    public class Vertex
+    /// <typeparam name="TData">The type of the data that the vertex store.</typeparam>
+    public class Vertex<TData>
     {
         /// <summary>
-        /// The field used to store the Vertex's label.
+        /// The field used to store the Vertex's data.
         /// </summary>
-        private readonly string _label;
+        private readonly TData _data;
 
         /// <summary>
-        /// Default Vertex constructor. Will create a Vertex with a label equal to <see cref="string.Empty"/>
+        /// Vertex constructor that allow the client to assign a data to the Vertex.
         /// </summary>
-        public Vertex() : this(string.Empty) { }
-
-        /// <summary>
-        /// Vertex constructor that allow the client to set a specific label to the Vertex.
-        /// </summary>
-        /// <param name="label">The label that the client will apply to the Vertex.</param>
-        /// <exception cref="ArgumentNullException">
-        /// If <paramref name="label"/> is equal to null.
-        /// </exception>
-        public Vertex(string label)
+        /// <param name="data">The data to store in the vertex.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="data"/> is equal to <see cref="null"/>.</exception>
+        public Vertex(TData data)
         {
-            if (label == null)
+            if (data == null)
             {
-                throw new ArgumentNullException(nameof(label));
+                throw new ArgumentNullException(nameof(data));
             }
 
-            _label = label;
+            _data = data;
         }
 
         /// <summary>
-        /// The Vertex's label.
+        /// The Vertex's data.
         /// </summary>
-        public string Label { get { return _label; } }
+        public TData Data { get { return _data; } }
+
+        /// <summary>
+        /// Create and return a <see cref="string"/> representation of the current <see cref="Vertex{TData}"/>.
+        /// </summary>
+        /// <returns>The <see cref="string"/> representation of the current <see cref="Vertex{TData}"/>.</returns>
+        public override string ToString()
+        {
+            return _data.ToString();
+        }
     }
 }
