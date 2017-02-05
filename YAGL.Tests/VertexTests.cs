33e53b72b33d7cb192bdac8b17936d596d87d8ef
thomasperez <thomas_perez@orange.fr>
2017-02-05 19:21:25 +0100

    Vextex class

---
+++ YAGL.Tests/VertexTests.cs
@@ -0,0 +1,32 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System;
+
+namespace YAGL.Tests
+{
+    [TestFixture]
+    public class VertexTests
+    {
+        [Test]
+        public void Vertex_Label_should_return_string_Empty_if_no_label_has_been_passed_to_the_constructor()
+        {
+            Vertex vertex = new Vertex();
+
+            Check.That(vertex.Label).IsEqualTo(string.Empty);
+        }
+
+        [Test]
+        public void Vertex_Label_should_return_the_label_that_has_been_passed_to_the_constructor()
+        {
+            Vertex vertex = new Vertex("42");
+
+            Check.That(vertex.Label).IsEqualTo("42");
+        }
+
+        [Test]
+        public void Vertex_constructor_should_throws_a_a_ArgumentNullException_if_the_label_passed_to_the_constructo_is_null()
+        {
+            Check.ThatCode(() => new Vertex(null)).Throws<ArgumentNullException>();
+        }
+    }
+}
---
+++ YAGL/Vertex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace YAGL
+{
+    /// <summary>
+    /// The class <see cref="Vertex"/> model the fundamental unit of wich graphs are formed in Graph Theory.
+    /// </summary>
+    public class Vertex
+    {
+        /// <summary>
+        /// The field used to store the Vertex's label.
+        /// </summary>
+        private readonly string _label;
+
+        /// <summary>
+        /// Default Vertex constructor. Will create a Vertex with a label equal to <see cref="string.Empty"/>
+        /// </summary>
+        public Vertex() : this(string.Empty) { }
+
+        /// <summary>
+        /// Vertex constructor that allow the client to set a specific label to the Vertex.
+        /// </summary>
+        /// <param name="label">The label that the client will apply to the Vertex.</param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="label"/> is equal to null.
+        /// </exception>
+        public Vertex(string label)
+        {
+            if (label == null)
+            {
+                throw new ArgumentNullException(nameof(label));
+            }
+
+            _label = label;
+        }
+
+        /// <summary>
+        /// The Vertex's label.
+        /// </summary>
+        public string Label { get { return _label; } }
+    }
+}
