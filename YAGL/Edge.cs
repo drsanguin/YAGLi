02b7d330aeaa040d6ca19ec72aa1da00914327c6
thomasperez <thomas_perez@orange.fr>
2017-02-12 17:42:14 +0100

    renamed TData to TVertex

--- YAGL/Edge.cs
+++ YAGL/Edge.cs
@@ -4,30 +4,30 @@ using System.Collections.Generic;
 namespace YAGL
 {
     /// <summary>
-    /// The class <see cref="Edge{TData}"/> models the fundamental unit of wich graphs are formed in Graph Theory.
+    /// The class <see cref="Edge{TVertex}"/> models the fundamental unit of wich graphs are formed in Graph Theory.
     /// </summary>
-    /// <typeparam name="TData">The type of the vertices.</typeparam>
-    public class Edge<TData>
+    /// <typeparam name="TVertex">The type of the vertices.</typeparam>
+    public class Edge<TVertex>
     {
         /// <summary>
-        /// The field that store the first of the two <see cref="Edge{TData}"/> ends.
+        /// The field that store the first of the two <see cref="Edge{TVertex}"/> ends.
         /// </summary>
-        private TData _end1;
+        private TVertex _end1;
 
         /// <summary>
-        /// The field that store the second of the two <see cref="Edge{TData}"/> ends.
+        /// The field that store the second of the two <see cref="Edge{TVertex}"/> ends.
         /// </summary>
-        private TData _end2;
+        private TVertex _end2;
 
         /// <summary>
-        /// <see cref="Edge{TData}"/> class constructor that allow the client to assign the two ends of the <see cref="Edge{TData}"/> object.
+        /// <see cref="Edge{TVertex}"/> class constructor that allow the client to assign the two ends of the <see cref="Edge{TVertex}"/> object.
         /// </summary>
-        /// <param name="end1">The first of the two <see cref="Edge{TData}"/> ends.</param>
-        /// <param name="end2">The second of the two <see cref="Edge{TData}"/> ends.</param>
+        /// <param name="end1">The first of the two <see cref="Edge{TVertex}"/> ends.</param>
+        /// <param name="end2">The second of the two <see cref="Edge{TVertex}"/> ends.</param>
         /// <exception cref="ArgumentNullException">
         /// Either if <paramref name="end1"/> or <paramref name="end2"/> is equal to <see cref="null"/>.
         /// </exception>
-        public Edge(TData end1, TData end2)
+        public Edge(TVertex end1, TVertex end2)
         {
             if (end1 == null)
             {
@@ -46,27 +46,27 @@ namespace YAGL
         /// <summary>
         /// The vertices end of this instance.
         /// </summary>
-        public IEnumerable<TData> Ends { get { yield return _end1; yield return _end2; } }
+        public IEnumerable<TVertex> Ends { get { yield return _end1; yield return _end2; } }
 
         /// <summary>
         /// Determine if the paramter <paramref name="obj"/> is equal to this instance.
-        /// The result of this method is equivalent to calling the method <see cref="Equals(Edge{TData}, EdgeComparison)"/> with the value <see cref="EdgeComparison.ConsiderDirection"/>.
+        /// The result of this method is equivalent to calling the method <see cref="Equals(Edge{TVertex}, EdgeComparison)"/> with the value <see cref="EdgeComparison.ConsiderDirection"/>.
         /// </summary>
         /// <param name="obj">The other object to compare to this instance.</param>
         /// <returns><see cref="true"/> if the object <paramref name="obj"/> is equal to this instance. <see cref="false"/> otherwise.</returns>
         public override bool Equals(object obj)
         {
-            return Equals(obj as Edge<TData>, EdgeComparison.ConsiderDirection);
+            return Equals(obj as Edge<TVertex>, EdgeComparison.ConsiderDirection);
         }
 
         /// <summary>
         /// Determine if the parameter <paramref name="other"/> is equal to this instance.
         /// </summary>
-        /// <param name="other">The other <see cref="Edge{TData}"/> object to compare to this instance.</param>
-        /// <param name="comparisonType">One of the value of <see cref="EdgeComparison"/> who determine the way of comparing the two <see cref="Edge{TData}"/> objects.</param>
+        /// <param name="other">The other <see cref="Edge{TVertex}"/> object to compare to this instance.</param>
+        /// <param name="comparisonType">One of the value of <see cref="EdgeComparison"/> who determine the way of comparing the two <see cref="Edge{TVertex}"/> objects.</param>
         /// <returns><see cref="true"/> if the parameter <paramref name="other"/> is equal to this instance. <see cref="false"/> otherwise.</returns>
         /// <exception cref="ArgumentException"> If <paramref name="comparisonType"/> is not a value from <see cref="EdgeComparison"/>.</exception>
-        public bool Equals(Edge<TData> other, EdgeComparison comparisonType)
+        public bool Equals(Edge<TVertex> other, EdgeComparison comparisonType)
         {
             if (other == null)
             {
@@ -94,15 +94,15 @@ namespace YAGL
         /// <summary>
         /// Determine if the parameter <paramref name="other"/> have the same references setted to his fields as this instance.
         /// </summary>
-        /// <param name="other">The other <see cref="Edge{TData}"/> object to compare to this instance.</param>
+        /// <param name="other">The other <see cref="Edge{TVertex}"/> object to compare to this instance.</param>
         /// <returns><see cref="true"/> if the parameter <paramref name="other"/> is equal to this instance. <see cref="false"/> otherwise.</returns>
-        private bool haveTheSameFieldsReferences(Edge<TData> other)
+        private bool haveTheSameFieldsReferences(Edge<TVertex> other)
         {
             return _end1.Equals(other._end1) && _end2.Equals(other._end2);
         }
 
         /// <summary>
-        /// The <see cref="Edge{TData}"/> hashing method.
+        /// The <see cref="Edge{TVertex}"/> hashing method.
         /// </summary>
         /// <returns>The hashed value of this instance.</returns>
         public override int GetHashCode()
