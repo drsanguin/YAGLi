92df5a5b84e1356c1f2d675d304d549e0e8d6515
thomasperez <thomas_perez@orange.fr>
2017-07-02 13:00:50 +0200

    Refactoring specs: created StepTransforms

---
+++ tests/YAGLi.Specs/Transforms/EdgeTransforms.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using YAGLi.Tests.Utils;
+
+namespace YAGLi.Specs.Transforms
+{
+    [Binding]
+    public class EdgeTransforms
+    {
+        private const string EDGE_END1_PROPERTY_NAME = nameof(Edge<Vertex>.End1);
+        private const string EDGE_END2_PROPERTY_NAME = nameof(Edge<Vertex>.End2);
+
+        [StepArgumentTransformation]
+        public IEnumerable<Edge<Vertex>> EdgesTransform(Table table)
+        {
+            return table.Rows.Select(row => new Edge<Vertex>(row[EDGE_END1_PROPERTY_NAME], row[EDGE_END2_PROPERTY_NAME]));
+        }
+
+        [StepArgumentTransformation]
+        public Edge<Vertex> EdgeTransform(Table table)
+        {
+            var firstRow = table.Rows[0];
+
+            return new Edge<Vertex>(firstRow[EDGE_END1_PROPERTY_NAME], firstRow[EDGE_END2_PROPERTY_NAME]);
+        }
+    }
+}
---
+++ tests/YAGLi.Specs/Transforms/VertexTransforms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using YAGLi.Tests.Utils;
+
+namespace YAGLi.Specs.Transforms
+{
+    [Binding]
+    public class VertexTransforms
+    {
+        private const string VERTEX_NAME_PROPERTY_NAME = nameof(Vertex.Name);
+
+        [StepArgumentTransformation]
+        public Vertex VertexTransform(Table table)
+        {
+            var firstRow = table.Rows[0];
+
+            return firstRow[VERTEX_NAME_PROPERTY_NAME];
+        }
+
+        [StepArgumentTransformation]
+        public IEnumerable<Vertex> VerticesTransforms(Table table)
+        {
+            return table.Rows.Select(row => new Vertex(row[VERTEX_NAME_PROPERTY_NAME]));
+        }
+    }
+}
