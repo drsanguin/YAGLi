e7fa124cbe6499b26cb70e05a240973ccb7f7b8b
thomasperez <thomas_perez@orange.fr>
2017-07-17 00:02:21 +0200

    Forgot to add tests for AllowParalleEdges & AllowLoops

---
+++ tests/YAGLi.Tests/DirectedGraph/DirectedGraphTests.cs
@@ -0,0 +1,26 @@
+﻿using NFluent;
+using NUnit.Framework;
+using YAGLi.Tests.Utils;
+
+namespace YAGLi.Tests.DirectedGraph
+{
+    [TestFixture]
+    public class DirectedGraphTests
+    {
+        [Test]
+        public void DirectedGraph_AllowLoops_should_return_the_value_passed_in_the_constructor()
+        {
+            var graph = new DirectedGraph<Vertex, Edge<Vertex>>(true, true);
+
+            Check.That(graph.AllowLoops).IsTrue();
+        }
+
+        [Test]
+        public void DirectedGraph_AllowParallelEdges_should_return_the_value_passed_in_the_constructor()
+        {
+            var graph = new DirectedGraph<Vertex, Edge<Vertex>>(true, true);
+
+            Check.That(graph.AllowParallelEdges).IsTrue();
+        }
+    }
+}
