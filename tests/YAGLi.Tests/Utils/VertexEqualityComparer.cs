747561ac251c928e8a3d64224a992d86c0f4c310
thomasperez <thomas_perez@orange.fr>
2017-08-01 23:40:59 +0200

    Improved VertexEqualityComparer's Equals method

--- tests/YAGLi.Tests/Utils/VertexEqualityComparer.cs
+++ tests/YAGLi.Tests/Utils/VertexEqualityComparer.cs
@@ -6,6 +6,16 @@ namespace YAGLi.Tests.Utils
     {
         public bool Equals(Vertex vertex1, Vertex vertex2)
         {
+            if (ReferenceEquals(vertex1, vertex2))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(vertex1, null) || ReferenceEquals(vertex2, null))
+            {
+                return false;
+            }
+
             return vertex1.Name.Equals(vertex2.Name);
         }
 
