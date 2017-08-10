6ef1b58a48d58682160835127a64b1c316511905
thomasperez <thomas_perez@orange.fr>
2017-08-11 00:04:22 +0200

    ForEach extension

---
+++ src/YAGLi/Extensions/Collection/ForEachExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YAGLi.Extensions.Collection
+{
+    public static class ForEachExtension
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var element in source.ReplaceByEmptyIfNull())
+            {
+                action(element);
+            }
+
+            return source;
+        }
+    }
+}
---
+++ tests/YAGLi.Tests/Extensions/Collection/ForEachExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YAGLi.Extensions.Collection;
+
+namespace YAGLi.Tests.Extensions.Collection
+{
+    [TestFixture]
+    public class ForEachExtensionTests
+    {
+        [Test]
+        public void ForEachExtension_should_call_the_action_for_each_element_of_the_collection()
+        {
+            var collection = Enumerable.Range(0, 10);
+            var collection2 = new List<int>(collection.Count());
+            Action<int> action = x => collection2.Add(x);
+
+            collection.ForEach(action);
+
+            Check.That(collection2).ContainsExactly(collection);
+        }
+
+        [Test]
+        public void ForEachExtension_should_not_throw_with_a_null_collection()
+        {
+            IEnumerable<int> collection = null;
+            Action<int> action = x => { };
+
+            Check.ThatCode(() => collection.ForEach(action)).DoesNotThrow();
+        }
+    }
+}
