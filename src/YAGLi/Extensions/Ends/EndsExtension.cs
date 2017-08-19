9fbdfe458e0e781e8311b271cf0d3cb041f743cb
Perez Thomas <thomas_perez@orange.fr>
2017-08-19 23:25:42 +0200

    Introducing Ends extension

---
+++ src/YAGLi/Extensions/Ends/EndsExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using YAGLi.Extensions.Collection;
+using YAGLi.Interfaces;
+
+namespace YAGLi.Extensions.Ends
+{
+    public static class EndsExtension
+    {
+        public static IEnumerable<TVertex> Ends<TVertex>(this IModelAnEdge<TVertex> edge)
+        {
+            return edge.ends()
+                       .FilterNulls();
+        }
+
+        private static IEnumerable<TVertex> ends<TVertex>(this IModelAnEdge<TVertex> edge)
+        {
+            if (ReferenceEquals(edge, null))
+            {
+                yield break;
+            }
+
+            yield return edge.End1;
+
+            yield return edge.End2;
+        }
+    }
+}
---
+++ tests/YAGLi.Tests/Extensions/Edges/EndsExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using NFluent;
+using NUnit.Framework;
+using YAGLi.Extensions.Ends;
+using YAGLi.Tests.Utils;
+
+namespace YAGLi.Tests.Extensions.Edges
+{
+    [TestFixture]
+    public class EndsExtensionTests
+    {
+        [Test]
+        public void Ends_Extensions_should_return_the_expected_result()
+        {
+            var end1 = new Vertex("v0");
+            var end2 = new Vertex("v1");
+            var edge = new Edge<Vertex>(end1, end2);
+
+            Check.That(edge.Ends()).ContainsExactly(end1, end2);
+        }
+
+        [Test]
+        public void Ends_Extensions_with_a_null_edge_should_return_the_expected_result()
+        {
+            Edge<Vertex> edge = null;
+
+            Check.That(edge.Ends()).IsEmpty();
+        }
+
+        [Test]
+        public void Ends_Extensions_with_an_edge_with_null_ends_should_return_an_empty_IEnumerable()
+        {
+            Edge<Vertex> edge = new Edge<Vertex>(null, null);
+
+            Check.That(edge.Ends()).IsEmpty();
+        }
+    }
+}
